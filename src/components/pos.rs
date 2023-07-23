421ee9f896c09640a1ba93c6b6777046ada86559
thesimplekid <tsk@thesimplekid.com>
2023-07-22 20:52:35 -0400

    max width for set_mint and set_rec_key

--- src/components/pos.rs
+++ src/components/pos.rs
@@ -46,17 +46,17 @@ impl Component for Pos {
     fn view(&self, ctx: &Context<Self>) -> Html {
         let set_pubkey = ctx.link().callback(|_| Msg::AmountSubmitted);
         html! {
-            <>
-            <a class="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-
-                <div class="relative z-0 w-full mb-6 group">
+          <div class="flex justify-center">
+            <a class="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-96 max-w-lg">
+              <div class="relative z-0 w-full mb-6 group">
                     <label for="amount" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{"Amount (sats)"}</label>
                     <input type="numeric" name="amount" id="amount" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" ref={self.amount_node_ref.clone()} />
+                <div class="flex justify-center">
                     <button class="px-6 py-2 rounded-sm shadow-sm dark:bg-violet-400 dark:text-gray-900" onclick={set_pubkey}>{"Create Invoice"}</button>
                 </div>
-
+              </div>
             </a>
-            </>
+          </div>
         }
     }
 }
--- src/components/set_mint.rs
+++ src/components/set_mint.rs
@@ -49,14 +49,18 @@ impl Component for SetMint {
         let set_pubkey = ctx.link().callback(|_| Msg::PubkeySubmitted);
         html! {
             <>
-            <a class="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <div class="relative z-0 w-full mb-6 group">
-                <label for="description" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{"Description"}</label>
-                <input type="text" name="description" id="description" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" ref={self.mint_node_ref.clone()} />
-                <button class="px-6 py-2 rounded-sm shadow-sm dark:bg-violet-400 dark:text-gray-900" onclick={set_pubkey}>{"Set Mint"}</button>
-            </div>
+          <div class="flex justify-center">
+            <a class="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-96 max-w-lg">
+              <div class="relative z-0 w-full mb-6 group">
+                <label for="mint_url" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{"Mint Url"}</label>
+                <input type="text" name="mint_url" id="mint_url" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" ref={self.mint_node_ref.clone()} />
+                <div class="flex justify-center">
+                <button class="px-6 py-2 mt-2 rounded-sm shadow-sm dark:bg-violet-400 dark:text-gray-900" onclick={set_pubkey}>{"Set Mint"}</button>
+                </div>
+              </div>
             </a>
-            </>
+          </div>
+        </>
         }
     }
 }
