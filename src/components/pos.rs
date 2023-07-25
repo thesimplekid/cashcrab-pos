e540542bcfc8b2ec66a8f2a4c0281a5b4791d162
thesimplekid <tsk@thesimplekid.com>
2023-07-25 15:15:58 -0400

    input usd and convert to sats

--- src/components/pos.rs
+++ src/components/pos.rs
@@ -15,6 +15,7 @@ pub enum Msg {
     AmountSubmitted,
     ButtonPressed(i32),
     GotPrice(u64),
+    EnterFiat,
 }
 
 #[derive(Default)]
@@ -22,6 +23,8 @@ pub struct Pos {
     amount: String,
     sat_per_usd: Option<u64>,
     fiat_value: String,
+    enter_fiat: bool,
+    disable_decimal: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -47,6 +50,22 @@ async fn get_price(price_cb: Callback<u64>) -> Result<()> {
     Ok(())
 }
 
+fn format_fiat(amount: &str) -> String {
+    let amount = amount.replace('.', "").trim_start_matches('0').to_string();
+    let num_zeros = if amount.len().lt(&3) {
+        3 - amount.len()
+    } else {
+        0
+    };
+
+    let formated_amount = format!("{}{}", "0".repeat(num_zeros), amount);
+    let decimal_pos = formated_amount.len() - 2;
+    let mut result = formated_amount;
+    result.insert(decimal_pos, '.');
+
+    result
+}
+
 impl Component for Pos {
     type Message = Msg;
     type Properties = Props;
@@ -60,7 +79,7 @@ impl Component for Pos {
 
         Self {
             amount: "0".to_string(),
-            fiat_value: "0".to_string(),
+            fiat_value: "0.00".to_string(),
             ..Default::default()
         }
     }
@@ -79,28 +98,69 @@ impl Component for Pos {
                 true
             }
             Msg::ButtonPressed(button_num) => {
-                log::debug!("{}", button_num);
+                let edit_amount = match self.enter_fiat {
+                    true => &self.fiat_value,
+                    false => &self.amount,
+                };
 
-                if button_num.eq(&12) {
-                    self.amount.pop();
-                    if self.amount.eq("") {
-                        self.amount = format!("{}", 0);
+                let new_amount = match button_num {
+                    12 => {
+                        let mut edit_amount = edit_amount.clone();
+                        let popped = edit_amount.pop();
+                        if self.enter_fiat {
+                            format_fiat(&edit_amount)
+                        } else if edit_amount.is_empty() {
+                            "0".to_string()
+                        } else if let Some(ch) = popped {
+                            if ch == '.' {
+                                self.disable_decimal = false;
+                            }
+                            edit_amount
+                        } else {
+                            edit_amount
+                        }
                     }
+                    10 => {
+                        self.disable_decimal = true;
+                        format!("{}.", edit_amount)
+                    }
+                    11 => {
+                        if edit_amount == "0" {
+                            "0".to_string()
+                        } else {
+                            format!("{}{}", edit_amount, button_num)
+                        }
+                    }
+                    _ => {
+                        let button_num = if button_num == 11 { 0 } else { button_num };
+                        let new_amount = format!("{}{}", edit_amount, button_num);
+
+                        if self.enter_fiat {
+                            format_fiat(&new_amount)
+                        } else {
+                            new_amount
+                        }
+                    }
+                };
+
+                if self.enter_fiat {
+                    if let (Ok(amount), Some(sat_per_usd)) =
+                        (new_amount.parse::<f64>(), self.sat_per_usd)
+                    {
+                        let value = (sat_per_usd as f64 * amount).round() as u64;
+                        self.amount = value.to_string();
+                    }
+
+                    self.fiat_value = new_amount;
                 } else {
-                    let button_num = if button_num.eq(&11) { 0 } else { button_num };
-
-                    if self.amount.eq("0") {
-                        self.amount = format!("{}", button_num);
-                    } else {
-                        self.amount = format!("{}{}", self.amount, button_num);
+                    if let (Ok(amount), Some(sat_per_usd)) =
+                        (new_amount.parse::<u64>(), self.sat_per_usd)
+                    {
+                        let value = ((amount as f64 / sat_per_usd as f64) * 100.0).round() / 100.0;
+                        self.fiat_value = format_fiat(&value.to_string());
                     }
-                }
 
-                if let (Ok(amount), Some(sat_per_usd)) =
-                    (self.amount.parse::<u64>(), self.sat_per_usd)
-                {
-                    let value = ((amount as f64 / sat_per_usd as f64) * 100.0).round() / 100.0;
-                    self.fiat_value = value.to_string();
+                    self.amount = new_amount.trim_start_matches('0').to_string();
                 }
 
                 true
@@ -111,61 +171,97 @@ impl Component for Pos {
                 self.sat_per_usd = Some(sats_per_dollar);
                 true
             }
+            Msg::EnterFiat => {
+                self.enter_fiat = !self.enter_fiat;
+                true
+            }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let set_pubkey = ctx.link().callback(|_| Msg::AmountSubmitted);
-        let l = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        let l = (1..=12).collect::<Vec<_>>();
 
+        let enter_fiat_cb = ctx.link().callback(|_| Msg::EnterFiat);
         html! {
-          <div class="flex justify-center">
-            <a class="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-96 lg:max-w-lg sm:w-full">
+                  <div class="flex justify-center">
+                    <a class="block flex-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-96 lg:max-w-lg sm:w-full">
 
-            <h1 class="text-3xl mb-4 font-semibold leadi text-center">{format!("{} sats", self.amount.clone())}</h1>
-            <h1 class="text-3xl mb-4 font-light leadi text-center">{format!("{} USD", self.fiat_value.clone())}</h1>
-            <div class="grid grid-cols-3 gap-4">
+        <div class="grid grid-rows-2 grid-flow-col gap-2">
+            <div class="row-span-2 col-span-1 px-8 py-4"></div>
+                    {
+                        match self.enter_fiat {
+                            true => {
+                                html! {
+                                <>
+                                    <h1 class="row-span-1 col-span-8 text-3xl mb-4 font-light leadi text-center">{format!("${}", self.fiat_value.clone())}</h1>
+                                    <h1 class="row-span-1 col-span-8 text-3xl mb-4 font-semibold leadi text-center">{format!("{} sats", self.amount.clone())}</h1>
+                                </>
+                                }
 
-            {
-                l.into_iter().map(|i| {
-                    let cb = ctx.link().callback(move |_| Msg::ButtonPressed(i ));
-                    if i == 10 {
-                        html!{
-                            <>
-                            <div></div>
-                            </>
-                        }
-                    } else if i == 11 {
-                        html!{
-                            <>
-                            <button class="px-8 py-4 rounded-sm shadow-lg dark:bg-violet-400 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={cb}>{"0"}</button>
-                            </>
-                        }
-                    } else if i == 12 {
-                        html! {
-                            <>
-                            <button class="px-8 py-4 rounded-sm shadow-lg dark:bg-violet-400 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={cb}>{"X"}</button>
-                            </>
-                        }
-                    } else {
-                        html! {
-                            <>
-                            <button class="px-8 py-4 rounded-sm shadow-lg dark:bg-violet-400 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={cb}>{i}</button>
-                            </>
+                            }
+                            false => {
+                                html! {
+                                    <>
+                                        <h1 class="row-span-1 col-span-8 text-3xl mb-4 font-semibold leadi text-center">{format!("{} sats", self.amount.clone())}</h1>
+                                        <h1 class="row-span-1 col-span-8 text-3xl mb-4 font-light leadi text-center">{format!("${}", self.fiat_value.clone())}</h1>
+                                    </>
+                                }
+
+                            }
                         }
                     }
-                }).collect::<Html>()
-            }
-            </div>
+                    <button class="row-span-2 col-span-1 px-8 py-4" onclick={enter_fiat_cb}><svg viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" class="stroke-violet-400 h-1/2"><path d="M9.019 9A6.5 6.5 0 1115 14.981M8.5 22a6.5 6.5 0 110-13 6.5 6.5 0 010 13zM22 17a3 3 0 01-3 3h-2m0 0l2-2m-2 2l2 2M2 7a3 3 0 013-3h2m0 0L5 6m2-2L5 2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>
+                    </div>
+                    <div class="grid grid-cols-3 gap-4">
 
-              <div class="relative z-0 w-full mb-6 group">
-                    // <input type="numeric" name="amount" id="amount" class="block py-4 px-6 w-full lg:text-lg sm:text-5xl text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer" placeholder={"Amount (sats)"} ref={self.amount_node_ref.clone()} />
-                <div class="flex justify-center">
-                    <button class="px-8 py-4 mt-5 w-full rounded-sm shadow-lg dark:bg-green-600 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={set_pubkey}>{"Create Invoice"}</button>
-                </div>
-              </div>
-            </a>
-          </div>
-        }
+                    {
+                        l.into_iter().map(|i| {
+                            let cb = ctx.link().callback(move |_| Msg::ButtonPressed(i ));
+                            if i == 10 {
+                                if self.enter_fiat {
+                                    html!{<div></div>
+                                }
+
+                                } else {
+                                html!{
+                                    <>
+                                    <div></div>
+                                    </>
+                                }
+                                }
+                            } else if i == 11 {
+                                html!{
+                                    <>
+                                    <button class="px-8 py-4 rounded-sm shadow-lg dark:bg-violet-400 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={cb}>{"0"}</button>
+                                    </>
+                                }
+                            } else if i == 12 {
+                                html! {
+                                    <>
+                                    <button class="px-8 py-4 rounded-sm shadow-lg dark:bg-violet-400 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={cb}>{"X"}</button>
+                                    </>
+                                }
+                            } else {
+                                html! {
+                                    <>
+                                    <button class="px-8 py-4 rounded-sm shadow-lg dark:bg-violet-400 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={cb}>{i}</button>
+                                    </>
+                                }
+                            }
+                        }).collect::<Html>()
+                    }
+
+                    </div>
+
+                      <div class="relative z-0 w-full mb-6 group">
+                            // <input type="numeric" name="amount" id="amount" class="block py-4 px-6 w-full lg:text-lg sm:text-5xl text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer" placeholder={"Amount (sats)"} ref={self.amount_node_ref.clone()} />
+                        <div class="flex justify-center">
+                            <button class="px-8 py-4 mt-5 w-full rounded-sm shadow-lg dark:bg-green-600 dark:text-gray-900 lg:text-lg sm:text-5xl font-medium" onclick={set_pubkey}>{"Create Invoice"}</button>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                }
     }
 }
